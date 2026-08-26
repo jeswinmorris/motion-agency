e06846f25fff3a3e8bba075a992c9205eb90a1ea
Jeswin Morris <jeswinmorrisfl@gmail.com>
2026-08-27 01:05:00 +0530

    Add GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,33 @@
 import "./App.css";
+
 import CustomCursor from "./components/CustomCursor";
+import SmoothScroll from "./components/SmoothScroll";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Showreel from "./components/Showreel";
 import Work from "./components/Work";
 import Services from "./components/Services";
+import Process from "./components/Process";
+import About from "./components/About";
+import FinalCTA from "./components/FinalCTA";
+import ProjectForm from "./components/ProjectForm";
 
 function App() {
   return (
     <main className="app">
+      <SmoothScroll />
       <CustomCursor />
-      <nav className="navbar">
-        <div className="logo">
-          CORTOL<span>STUDIO</span>
-        </div>
 
-        <div className="nav-links">
-          <a href="#work">Work</a>
-          <a href="#services">Services</a>
-          <a href="#showreel">Showreel</a>
-          <a href="#about">About</a>
-        </div>
-
-        <a href="#contact" className="nav-cta">
-          Start a project
-        </a>
-      </nav>
+      <Navbar />
 
       <Hero />
-
       <Showreel />
-
       <Work />
-
       <Services />
+      <Process />
+      <About />
+      <FinalCTA />
+      <ProjectForm />
     </main>
   );
 }
